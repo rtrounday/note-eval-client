045e86cc85c39eec66f87430158114d85f4423a3
Sandeep Raghunandhan <sraghunandhan@shutterfly.com>
2016-08-18 20:00:25 -0700

    Implemented view uploaded files featue

--- js/retriveUser.js
+++ js/retriveUser.js
@@ -1,12 +1,17 @@
-$(document).ready(function(){
+function goHome(){
   firebase.auth().onAuthStateChanged(function(user){
     if(user){
       var userUid = user.uid;
       firebase.database().ref('users/' + userUid + '/username').on(
         'value', function(snapshot){
           $("#welcome").text("Hello, " + snapshot.val());
+          $("#welcome").show();
         }
       );
+      $("#filesView").hide();
     }
-  })
+  });
+}
+$(document).ready(function(){
+  goHome();
 });
--- js/viewFiles.js
+++ js/viewFiles.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
+  $("#viewFiles").one('click', function(){
+    $('#filesView').show();
+    firebase.auth().onAuthStateChanged(function(user){
+      if (user)
+      {
+        var userUid = user.uid;
+        firebase.database().ref('users/' + userUid + "/files").on('value', function(snapshot){
+            $("#filesView").empty();
+            snapshot.forEach(function(file){
+              var fileName = file.child("name").val();
+              var fileURL = file.child("file_archive").val();
+              var content = "<a href=" + fileURL + ">" + fileName + "</a></br>";
+              $("#filesView").append(content);
+            });
+        });
+      }
+    });
+  });
   $("#viewFiles").on('click', function(){
     $('#filesView').show();
+    $('#welcome').hide();
   });
 });
