b1adcda230164215b710040e07554f4d3d3b14c2
Sandeep Raghunandhan <sraghunandhan@shutterfly.com>
2016-08-18 23:31:28 -0700

    Fixed issues with upload task. Updated file view to Bootstrap
    Was not allowing multiple uploads. Better looking file list view using
    Bootstrap list-group. Also added duplicate checking functionality so
    that upload is stopped and user is told if the file they uploaded
    already exists.

--- js/viewFiles.js
+++ js/viewFiles.js
@@ -1,24 +1,30 @@
+function updateFiles(userUid)
+{
+  firebase.database().ref('users/' + userUid + "/files").on('value', function(snapshot){
+      $("#filesView").empty();
+      $("#filesView").append("<ul class='list-group'></ul>");
+      $(".list-group").empty();
+      snapshot.forEach(function(file){
+        var fileName = file.child("name").val();
+        var fileURL = file.child("file_archive").val();
+        var content = "<a href=" + fileURL + ">"
+                    + "<li class='list-group-item list-group-item-success'>" + fileName + "</li>"
+                    + "</a>";
+        $(".list-group").append(content);
+      });
+  });
+}
 $(document).ready(function(){
-  $("#viewFiles").one('click', function(){
+  $("#viewFiles").on('click', function(){
     $('#filesView').show();
+    $("#welcome").hide();
+    $("#filesView").append("<ul class='list-group'></ul>");
     firebase.auth().onAuthStateChanged(function(user){
       if (user)
       {
         var userUid = user.uid;
-        firebase.database().ref('users/' + userUid + "/files").on('value', function(snapshot){
-            $("#filesView").empty();
-            snapshot.forEach(function(file){
-              var fileName = file.child("name").val();
-              var fileURL = file.child("file_archive").val();
-              var content = "<a href=" + fileURL + ">" + fileName + "</a></br>";
-              $("#filesView").append(content);
-            });
-        });
+        updateFiles(userUid);
       }
     });
-  });
-  $("#viewFiles").on('click', function(){
-    $('#filesView').show();
-    $('#welcome').hide();
   });
 });
