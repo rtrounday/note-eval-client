624cc2576bbd5e453df2abc7d78ea2f07a73d2a0
Sandeep Raghunandhan <sraghunandhan@shutterfly.com>
2016-08-20 23:03:36 -0700

    Added form validation and more modern error message displaying

--- js/signup.js
+++ js/signup.js
@@ -1,9 +1,4 @@
-function signUpWithFirebase(form)
-{
-  var email = form.user.value;
-  var password = form.pass.value;
-  var first_name = form.firstname.value;
-  var last_name = form.lastname.value;
+function createUser(first_name, last_name, email, password, form){
   firebase.auth().createUserWithEmailAndPassword(email, password)
   .then(
     function(result){
@@ -16,37 +11,48 @@ function signUpWithFirebase(form)
     function(error){
     var errorCode = error.code;
     var errorMessage = error.message;
-    if (email == "")
-    {
-      return;
-    }
-    $(document).ready(function()
-    {
-      $("#errorMessage").text(errorMessage).css({"color": "red", "text-align": "center"})
-    }
+    $("#alert").show();
+    $("#alert").removeClass();
+    $("#alert").addClass("alert alert-danger");
+    $("#alertMessage").replaceWith("<p id='alertMessage'>"+ errorMessage + "</p>");
+  }
   );
-});
 }
-function signInWithFirebase(form)
-{
-  var email = form.user.value;
-  var password = form.pass.value;
+function login(email, password){
   firebase.auth().signInWithEmailAndPassword(email, password).then(
     function(result) {
       var username = email;
       document.location.href="authenticated.html";
     },
     function(error) {
-      // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
-      if (email == "")
-      {
-        return;
-      }
-      $(document).ready(function()
-      {
-        $("#errorMessage").text(errorMessage).css({"color": "red", "text-align": "center"})
-      });
+      $("#alert").show();
+      $("#alert").removeClass();
+      $("#alert").addClass("alert alert-danger");
+      $("#alertMessage").replaceWith("<p id='alertMessage'>"+ errorMessage + "</p>");
     });
+}
+function signUpWithFirebase(form)
+{
+  var email = form.user.value;
+  var password = form.pass.value;
+  var confirmPass = form.confirmPass.value;
+  var first_name = form.firstname.value;
+  var last_name = form.lastname.value;
+  // Validating the information on the form before submitting
+  var success = validateForm(email, password, first_name, last_name,  confirmPass);
+  if (success){
+    createUser(first_name, last_name, email, password, form);
+  }
+}
+function signInWithFirebase(form)
+{
+  var email = form.user.value;
+  var password = form.pass.value;
+  var success = validateForm(email, password);
+  if (success){
+    login(email, password);
+  }
+
   }
