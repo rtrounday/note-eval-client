7a9b18c901a75b6a7310e59e27a4410d3292fec7
Sandeep Raghunandhan <sraghunandhan@shutterfly.com>
2016-08-27 12:25:54 -0700

    Testing a post request

--- private/updatePrices.js
+++ private/updatePrices.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 $("#updatePrices").click(function(){
   var file = "http://ec2-52-26-13-186.us-west-2.compute.amazonaws.com/cgi-bin/helloworld.py"
-  $.ajax({method: "GET", url: file , success: function(result){
+  $.ajax({method: "POST", data: {"text": "Testing text"}, url: file , success: function(result){
     alert(result);
   }});
 });
