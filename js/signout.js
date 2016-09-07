64781e38bc6b16e2f256da0e8c412ce932896ed2
Sandeep Raghunandhan <swasandeep@yahoo.com>
2016-09-06 23:19:45 -0700

    Restructured and Verified Functionality of Springboot integration

--- js/signout.js
+++
@@ -1,9 +0,0 @@
-function signOut(){
-firebase.auth().signOut().then(function() {
-  // Sign-out successful.
-  alert("Logout successful");
-  document.location.href="index.html";
-}, function(error) {
-  console.log(error.errorMessage)
-});
-}
--- private/firebase_config.js
+++
@@ -1,8 +0,0 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyCVa3fDfH5m-Vp6rC4OrMj1GxaJmUpvRQw",
-  authDomain: "loannotesassistant.firebaseapp.com",
-  databaseURL: "https://loannotesassistant.firebaseio.com",
-  storageBucket: "loannotesassistant.appspot.com",
-};
-firebase.initializeApp(config); 
