b874230389d05722602d192fdf0331bb7bfca4ca
Sandeep Raghunandhan <swasandeep@yahoo.com>
2016-09-18 23:27:31 -0700

    Feedback for File Sync, Bug fix of automatically snapping to property listing

--- site/src/main/webapp/private/updatePrices.js
+++ site/src/main/webapp/private/updatePrices.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
   $("#filesView").on("click", ".fileSync", function(){
+    $("#notif").append(
+    "<div class='alert alert-success'>" +
+    "<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>" +
+    "<strong>Syncing file with analytics. Click My Notes to view progress.</strong>" +
+    "</div>");
     var file = $(this).attr("id")
+    // Access to the python server responsible for note analytics
     var priceUpdateScriptURL = "http://ec2-52-26-13-186.us-west-2.compute.amazonaws.com/cgi-bin/runUpdate.py"
     firebase.auth().onAuthStateChanged(function(user){
       var userUid = user.uid;
@@ -8,9 +14,23 @@ $(document).ready(function(){
       url: priceUpdateScriptURL,
       data: JSON.stringify({'file' : file + "", 'userUid' : userUid}),
       crossDomain: true,
+      // Notify User on Success and Failure
       success: function(result){
-        console.log(result);
-      }})
+        console.log("Done.")
+        $("#notif").append(
+        "<div class='alert alert-success'>" +
+        "<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>" +
+        "<strong>Sync Complete!</strong>" +
+        "</div>");
+      },
+      error: function(result){
+        $("#notif").append(
+        "<div class='alert alert-danger'>" +
+        "<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>" +
+        "<strong>Unable to process request at the moment</strong>" +
+        "</div>");
+      }
+      })
     });
   });
 });
