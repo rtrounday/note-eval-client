da62b86d76777d6611724b0992a3c2d73833e671
Sandeep Raghunandhan <saraghun@ucsd.edu>
2016-12-20 13:24:58 -0800

    Added Equity and LTV Computations

--- site/src/main/java/com/loannotes/site/NoteController.java
+++ site/src/main/java/com/loannotes/site/NoteController.java
@@ -1,4 +1,6 @@
 package com.loannotes.site;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
 
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class NoteController {
 	private final String API_SECRET = "ZFwdl0C58Lx7GMxYuEpFCTeBZSdKIZHK4FtKd8f2"; 
 	private final String databaseURLTemplate = "https://loannotesassistant.firebaseio.com/users/%s/notes/%s.json?auth=%s"; 
+	private final NumberFormat formatter = new DecimalFormat("#0.00");
 	/*
 	 * Given a userUID and a loan note UID, queries the Google Firebase database 
 	 * for detailed information regarding the note. 
@@ -39,6 +42,22 @@ public class NoteController {
 	public String getNote(@RequestParam(value="userId", required=true) String userId,
 			@RequestParam(value="noteId", required=true) String noteId, Model model){
 		TreeMap<String, Object> note = getNoteDetails(userId, noteId); 
+		// Compute and add the equity 
+		if (note.get("Equity") == null && note.get("Zillow") != null && note.get("CurrentUPB") != null){
+			double unpaidBalance = Double.parseDouble((String)note.get("CurrentUPB")); 
+			double currentPrice = Double.parseDouble(((String)note.get("Zillow")).substring(1)); 
+			note.put("Equity", formatter.format(unpaidBalance - currentPrice));
+		}
+		if (note.get("Equity") != null){
+			double equity = Double.parseDouble((String)note.get("Equity")); 
+			note.put("Equity", formatter.format(equity));
+		}
+		// Compute and add LTV
+		if (note.get("LTV") == null && note.get("Zillow") != null && note.get("CurrentUPB") != null){
+			double unpaidBalance = Double.parseDouble((String)note.get("CurrentUPB")); 
+			double currentPrice = Double.parseDouble(((String)note.get("Zillow")).substring(1)); 
+			note.put("LTV", formatter.format(unpaidBalance/currentPrice));
+		}
 		model.addAllAttributes(note);
 		return "note"; 
 	}
